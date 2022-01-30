c30477dd27f8c6daaacee6c57af2cca590fff89d
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-01-30 21:05:17 +0200

    feat:add solutions

--- app.js
+++ app.js
@@ -1,45 +1,19 @@
 // solution 1
-function likes(names) {
-    const [firstN, secondN, ...rest] = names;
-    return  !names.length? 'no one likes this'
-      : names.length == 1? `${firstN} likes this` 
-      : names.length == 2? `${firstN} and ${secondN} like this` 
-      : names.length == 3? `${firstN}, ${secondN} and ${rest} like this`
-      : `${firstN}, ${secondN} and ${rest.length} others like this`;
-  }
+const countBits = n=>n.toString(2).split('0').join('').length
 // solution 2
-function likes(names) {
-    let a = " like this";
-    let b = " likes this";
-    let l = names.length;
-    return !l ? "no one" + b
-     : l == 1 ? names[0] + b
-     : l == 2 ? names.join(" and ") + a
-     : l == 3 ? names.join(" and ").replace(" and", ",") + a
-     : names[0] + ", " + names[1] + " and " + (l-2) + " others" + a;
-  }
+const countBits = function(n) {
+      return n.toString(2).replace(/0/g,'').length;
+    };
 
 // solution 3
-function likes(names) {
-    var a = names.length;
-    switch (a) {
-        case 0: return "no one likes this"; break;
-        case 1: return `${names[0]} likes this`; break;
-        case 2: return `${names[0]} and ${names[1]} like this`; break;
-        case 3: return `${names[0]}, ${names[1]} and ${names[2]} like this`; break;
-        default: return `${names[0]}, ${names[1]} and ${a-2} others like this`; break;
-    }
-  }
+const countBits = n => n.toString(2)
+                        .split('')
+                        .filter(ele => ele == 1)
+                        .length
 // solution 4
 
-function likes(names) {
-    const sum=names.length
-     if (sum===0) return 'no one likes this';
-     if (sum===1) return `${names[0]} likes this`;
-     if (sum===2) return `${names[0]} and ${names[1]} like this`;
-     if (sum===3) return `${names[0]}, ${names[1]} and ${names[2]} like this`;
-     return `${names[0]}, ${names[1]} and ${sum-2} others like this`;
-   }
+var countBits = function(n) {
+      return (n.toString(2).match(/1/g) || []).length;
+    };
 
-console.log(likes(['Peter']));
-console.log(likes(['Alex', 'Jacob', 'Mark', 'Max']));
+console.log(countBits(12345));
